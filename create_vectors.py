1f2209f2e7056760c2ceb7456057e9006b3f45ee
Juan Scavuzzo <juansca@localhost.localdomain>
2018-02-09 09:37:50 -0300

    Little change on output model path. Error msg on concat_files

--- create_vectors.py
+++ create_vectors.py
@@ -1,59 +1,64 @@
 import fasttext
 import os
 
-case_deafault_1 = {
-    'output_file': 'vectors/100_1/out',
+case_default_1 = {
+    'output_dir': 'vectors/100_1/',
     'dim': 100,
     'word_ngram': 1,
 }
 
 case2 = {
-    'output_file': 'vectors/100_2/out',
+    'output_dir': 'vectors/100_2/',
     'dim': 100,
     'word_ngram': 2,
 }
 
 case3 = {
-    'output_file': 'vectors/100_3/out',
+    'output_dir': 'vectors/100_3/',
     'dim': 100,
     'word_ngram': 3,
 }
 
 
 case4 = {
-    'output_file': 'vectors/300_1/out',
+    'output_dir': 'vectors/300_1/',
     'dim': 300,
     'word_ngram': 1,
 }
 
 
 case5 = {
-    'output_file': 'vectors/100_1/out',
+    'output_dir': 'vectors/100_1/',
     'dim': 300,
     'word_ngram': 2,
 }
 
 case6 = {
-    'output_file': 'vectors/100_1/out',
+    'output_dir': 'vectors/100_1/',
     'dim': 300,
     'word_ngram': 3,
 }
 
 
-def create_word_vectors(input_file, output_file, dim, word_ngram):
+def create_word_vectors(input_file, output_dir, dim, word_ngram):
+
+    if not os.path.isdir(output_dir):
+        os.mkdir(output_dir)
+
+    output_file = os.path.join(output_dir, 'out')
 
     model = fasttext.skipgram(input_file, output_file,
-                              dim=dim, word_ngram=word_ngram)
+                              dim=dim, word_ngrams=word_ngram)
 
 
 if __name__ == '__main__':
 
     input_filename = 'input/input_vect_file.txt'
-    out_dir = 'vectors/'
+    root_out_dir = 'vectors/'
 
     assert os.path.isfile(input_filename)
 
-    if not os.path.isdir(out_dir):
-        os.mkdir(out_dir)
+    if not os.path.isdir(root_out_dir):
+        os.mkdir(root_out_dir)
 
-    create_word_vectors(input_filename, **case1)
+    create_word_vectors(input_filename, **case_default_1)
--- scripts/concat_files.py
+++ scripts/concat_files.py
@@ -5,15 +5,26 @@ def concat_files():
 
     root_clean_dir = 'cleaned_text/'
     output_file = 'input/input_vect_file.txt'
+    total_size = 0
 
     if not os.path.isdir('input'):
         os.mkdir('input')
 
     assert os.path.isdir(root_clean_dir)
 
+    print("All files are being concatenates...")
     with open(output_file, 'w') as outfile:
         for subdir, dirs, files in os.walk(root_clean_dir):
             for file in files:
-                with open(file) as infile:
+                my_file = os.path.join(subdir, file)
+                total_size += os.stat(my_file).st_size
+                with open(my_file, 'r') as infile:
                     for line in infile:
                         outfile.write(line)
+
+    error_msg = '''Something went wrong! The output file has not the correct total size'''
+    assert os.stat(output_file).st_size == total_size, error_msg
+    print("Contatenation is complete!")
+
+if __name__ == '__main__':
+    concat_files()
